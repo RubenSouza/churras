e256ff3792f8f1df6e98dae8c53f02c41e09c87c
RubenSouza <94180428+RubenSouza@users.noreply.github.com>
2021-12-04 14:17:15 -0300

    Atualização de design

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,16 @@
 //Refrigerante / agua - 1000 ml por pessoa + de 6 horas 1500ml
 //Crianças valem por 0,5 (meia pessoa)
 
+let click = document.getElementById("clicar");
+let esconder = document.getElementById("esconder")
+let botoes = document.getElementById("botoes_click")
 let inputAdultos = document.getElementById("adultos");
 let inputCriancas = document.getElementById("criancas");
 let inputDuracao = document.getElementById("duracao");
-
 let resultado = document.getElementById("resultado");
-let reset = document.getElementById = ("reset")
+let reset = document.getElementById = ("reset");
+
+
 
 function calcular(){
     console.log("Calculando")
@@ -20,9 +24,10 @@ function calcular(){
     let qtcerveja = cervejaPP(duracao) * adultos;
     let qtrefrigerantePP = refrigerantePP(duracao) * adultos + (refrigerantePP(duracao) / 2 * criancas);
 
-    resultado.innerHTML = `<p>${qtcarne/1000} Kg de Carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qtcerveja/355)} Latas de cerveja</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qtrefrigerantePP/2000)} Garrafas de Refrigerante 2l</p>`
+    resultado.innerHTML = `<img src="./assets/Logo.png">`
+    resultado.innerHTML += `<img class = "resultado_img" src="./assets/carne.png"><p class="linhas">${qtcarne/1000} Kg de Carne</p><br>`
+    resultado.innerHTML += `<img class = "resultado_img" src="./assets/cerveja.png"><p class="linhas">${Math.ceil(qtcerveja/355)} Latas de cerveja</p><br>`
+    resultado.innerHTML += `<img class = "resultado_img" src="./assets/refri.png"><p class="linhas">${Math.ceil(qtrefrigerantePP/2000)} Garrafas de Refrigerante 2l</p>`
 }
 function resetar(){
     reset = document.location.reload();
@@ -54,3 +59,16 @@ function refrigerantePP(duracao){
     }
 }
 
+function mudar() {
+    esconder.style.opacity = "0"
+}
+function aparecer(){
+    resultado.style.display = "block"
+}
+function botoes_click(){
+    botoes.style.opacity = "1"
+}
+
+click.addEventListener("click", mudar)
+click.addEventListener("click", aparecer)
+click.addEventListener("click", botoes_click)
